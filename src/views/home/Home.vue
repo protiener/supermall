166fa00760e42828baf36d6beb1456cdb48b24bb
protiener <986922520@qq.com>
2020-03-18 09:54:51 +0800

    '首页轮播图和推荐'

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,46 @@
 <template>
-  <div>首页</div>
+  <div id="home">
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners="banners"/>
+    <home-recommend :recommends="recommends"/>
+  </div>
 </template>
 
 <script>
+  import HomeSwiper from "./childComps/HomeSwiper";
+  import HomeRecommend from "./childComps/HomeRecommend";
+
+  import NavBar from "@/components/common/navbar/NavBar";
+
+  import {getHomeMultidata} from "@/network/home";
+
+
   export default {
-    name: "Home"
+    name: "Home",
+    components: {
+      HomeSwiper,
+      HomeRecommend,
+      NavBar
+    },
+    data() {
+      return {
+        banners: [],
+        recommends: [],
+      }
+    },
+    created() {
+      getHomeMultidata().then(res => {
+        // console.log(res);
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      })
+    }
   }
 </script>
 
 <style scoped>
-
+  .home-nav {
+    background-color: var(--color-tint);
+    color: white;
+  }
 </style>
---
+++ src/views/home/childComps/HomeRecommend.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="recommend">
+    <div class="recommend-item" v-for="item in recommends">
+      <a :href="item.link">
+        <img :src="item.image" alt="">
+        <div>{{item.title}}</div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "HomeRecommend",
+    props: {
+      recommends: {
+        type: Array,
+        default() {
+          return [];
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .recommend {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-items: center;
+    text-align: center;
+  }
+
+  .recommend-item {
+    flex: 1;
+    margin: 8px 0;
+  }
+  .recommend-item img {
+    width: 70px;
+    height: 70px;
+    margin-bottom: 5px;
+  }
+  .recommend-item div {
+    font-size: var(--font-size);
+  }
+</style>
